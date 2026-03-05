21984c63542701160be6d49da4a1059724f44cbb
jzarcoo <zarco_romero_jose_antonio@comunidad.unam.mx>
2026-03-04 20:22:04 -0600

    feat(practice 2): Phase B

--- practice2/user_app.c
+++ practice2/user_app.c
@@ -6,20 +6,34 @@
 /* GPIO Button pin definition */
 #define BTN_PIN 15
 
+/**
+ * DELAY: Empty loop. We use 'volatile' to prevent the compiler 
+ * from optimizing it away.
+ * @param t: Number of iterations for the delay.
+ */
+void delay(int t){
+  for (volatile int x = 0; x < t; x ++) {
+	for (volatile int i = 0; i < 500000; i ++) ;
+  }
+}
+
 int user_app_entry(void) {
     // Setup Phase (Unprivileged): The user program configures its resources using syscalls.
-
-    // TODO: Configure LED_PIN as output and BTN_PIN as input using sys_gpio_dir syscall
     sys_gpio_dir(LED_PIN, 1);
     sys_gpio_dir(BTN_PIN, 0);
+    int led_state = 0;
+    int prev_btn_state = 0;
     while (1) {
-        // Operational Phase (Unprivileged): The user program performs its main functionality.
-        // TODO: Read the button state using sys_gpio_get 
-        // set the LED state accordingly using sys_gpio_set
         int btn_state = sys_gpio_get(BTN_PIN);
         sys_gpio_set(LED_PIN,btn_state);
+        // Falling edge detection for the button press
+        if (btn_state == 1 && prev_btn_state == 0) {
+            // Debounce
+            pausa(2);
+            led_state = !led_state; 
+            sys_gpio_set(LED_PIN, led_state);
+        }
+        prev_btn_state = btn_state;
     }
-    
-    // Should never reach here
     return 0;
 }
