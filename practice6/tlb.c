8f0669c6ca1ebeab7694edd3a3f95f183c8585ba
jzarcoo <zarco_romero_jose_antonio@comunidad.unam.mx>
2026-04-20 01:24:21 -0600

    p6: tlb

--- practice6/tlb.c
+++ practice6/tlb.c
@@ -18,6 +18,13 @@ int tlb_lookup(int page) {
     global_timer++;
     // TODO: Alumno - Buscar la pagina en el TLB
     // TODO: Alumno - Si se encuentra (HIT), actualizar last_access y retornar el frame
+    for (int i = 0; i < TLB_SIZE; i++) {
+        if (tlb[i].valid && tlb[i].page == page) {
+            tlb[i].last_access = global_timer; 
+            printf("TLB HIT: Page %d found in TLB (Frame %d)\n", page, tlb[i].frame);
+            return tlb[i].frame;
+        }
+    }
     // Si no se encuentra (MISS), notificar y retornar -1
     printf("TLB MISS: Page %d not in TLB\n", page);
     return -1;
@@ -27,8 +34,42 @@ void tlb_insert(int page, int frame) {
     global_timer++;
     
     // TODO: Alumno - Si el TLB no esta lleno, insertar en una entrada vacia
+    int entry_index = -1;
+    for (int i = 0; i < TLB_SIZE; i++) {
+        if (tlb[i].valid == 0) {
+            entry_index = i;
+            break;
+        }
+    }
+
     // TODO: Alumno - Si esta lleno, usar algoritmo LRU para desalojar una entrada
     // TODO: Alumno - En caso de empate en LRU, usar FIFO (usar insertion_time)
+    if (entry_index == -1) {
+
+        int lru_index = 0;
+
+        for (int i = 1; i < TLB_SIZE; i++) {
+            if (tlb[i].last_access < tlb[lru_index].last_access) {
+                lru_index = i;
+            } else if (tlb[i].last_access == tlb[lru_index].last_access) {
+                if (tlb[i].insertion_time < tlb[lru_index].insertion_time) {
+                    lru_index = i;
+                }
+            }
+        }
+        entry_index = lru_index;
+
+        printf("TLB EVICT: Entry %d (Page %d) discarded for Page %d\n",
+            lru_index, tlb[lru_index].page, page);
+    }
+
+    // Insertar la nueva entrada en el TLB
+    tlb[entry_index].valid = 1;
+    tlb[entry_index].page = page;
+    tlb[entry_index].frame = frame;
+    tlb[entry_index].last_access = global_timer;
+    tlb[entry_index].insertion_time = global_timer; 
+    global_timer++;
     
-    printf("TLB INSERT: Page %d -> Frame %d (Entry %d)\n", page, frame, 0 /* entry_index */);
+    printf("TLB INSERT: Page %d -> Frame %d (Entry %d)\n", page, frame, entry_index);
 }
